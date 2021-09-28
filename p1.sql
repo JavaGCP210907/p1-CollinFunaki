859eee78c43e74a885976e11df61b0697cfb2971
Collin Funaki <collinf@vt.edu>
2021-09-28 17:50:53 -0400

    Added hibernate xml file and logs file in src/main/resources and established a connection to my database

--- p1.sql
+++ p1.sql
@@ -1,16 +1,27 @@
+--Create Employee reimbursement system tables
+CREATE TABLE ers_user_roles (
+	user_role_id int PRIMARY KEY,
+	user_role varchar(10)
+);
+
 CREATE TABLE ers_users (
-	ers_users_id int PRIMARY KEY,
-	ers_username varchar(50),
-	ers_password varchar(50),
+	users_id int PRIMARY KEY,
+	username varchar(50),
+	password varchar(50),
 	user_first_name varchar(100),
 	user_last_name varchar(100),
 	user_email varchar(150),
-	user_role_id_fk int REFERENCES user_roles (ers_user_role_id)
+	user_role_id_fk int REFERENCES ers_user_roles (user_role_id)
 );
 
-CREATE TABLE user_roles (
-	ers_user_role_id int PRIMARY KEY,
-	user_role varchar(10)
+CREATE TABLE ers_reimbursement_type (
+	reimb_type_id int PRIMARY KEY,
+	reimb_type varchar(10)
+);
+
+CREATE TABLE ers_reimburement_status (
+	reimb_status_id int PRIMARY KEY,
+	reimb_status varchar(10)
 );
 
 CREATE TABLE ers_reimbursement (
@@ -26,20 +37,9 @@ CREATE TABLE ers_reimbursement (
 	reimb_type_id int
 );
 
-CREATE TABLE ers_reimbursement_type (
-	reimb_type_id int PRIMARY KEY,
-	reimb_type varchar(10)
-);
 
-CREATE TABLE ers_reimburement_status (
-	reimb_status_id int PRIMARY KEY,
-	reimb_status varchar(10)
-);
-
-
-
---DROP TABLE ers_users;
---DROP TABLE user_roles;
---DROP TABLE ers_reimbursement;
---DROP TABLE ers_reimbursement_type;
---DROP TABLE ers_reimburement_status;
+DROP TABLE ers_users;
+DROP TABLE ers_user_roles;
+DROP TABLE ers_reimbursement;
+DROP TABLE ers_reimbursement_type;
+DROP TABLE ers_reimburement_status;
--- src/main/java/com/revature/Launcher.java
+++ src/main/java/com/revature/Launcher.java
@@ -1,11 +1,24 @@
 package com.revature;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.revature.utils.HibernateUtil;
+
 public class Launcher {
 
 	public static void main(String[] args) {
 		
-		
+		try(Session ses = HibernateUtil.getSession()){
+			System.out.println("Hello you have a connection to your DB with Hibernate!");
+			HibernateUtil.closeSession();
+		} catch (HibernateException e) {
+			System.out.println("DB connection failed!!");
+		}
 
+		
+		
+		
 	}
 
 }
