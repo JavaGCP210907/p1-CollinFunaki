1008e483817aa994349ab6c2bc46139b315a3e66
Collin Funaki <collinf@vt.edu>
2021-10-05 18:03:42 -0400

    added/updated front end functionality and styling

---
+++ Front End/Employee.js
@@ -0,0 +1,14 @@
+const url = "http://localhost:3000/" //putting in our base URL
+
+//show past tickets when user click past tickets button
+document.getElementById("view").addEventListener(viewPast);
+//show add reimbursement form when user click add reimbursement button
+document.getElementById("add").addEventListener(addNew);
+
+function viewPast(){
+
+}
+
+function addNew(){
+    
+}
---
+++ Front End/Home.js
@@ -0,0 +1,14 @@
+
+//add functionality to our buttons using an eventListener
+//so when these buttons get clicked, the appropriate function below gets called
+//for home page
+document.getElementById("eloginbutton").addEventListener("click", employeeLogin);
+document.getElementById("mloginbutton").addEventListener("click", managerLogin);
+
+function employeeLogin(){
+    window.location.href = "Employee_Login.html"
+}
+
+function managerLogin(){
+    window.location.href = "Manager_Login.html"
+}
